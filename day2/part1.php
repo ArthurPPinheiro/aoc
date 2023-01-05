41668c5d83b30ea74dc7f4ca48d52ef69b9181bc
Arthur Pinheiro <arthurppinheiro@hotmail.com>
2023-01-05 16:45:40 -0300

    day 2

---
+++ day2/part1.php
@@ -0,0 +1,62 @@
+<?php
+
+    $file = fopen('/app/day2/input.txt', 'r');
+
+    $input = fread($file, filesize('/app/day2/input.txt'));
+
+    fclose($file);
+
+    $games = explode(PHP_EOL, $input);
+
+    $points = 0;
+
+    foreach($games as $game){
+        $choices = explode(" ", $game);
+
+        switch ($choices[0]) {
+            case 'A':
+                switch ($choices[1]) {
+                    case 'X':
+                        $points += 1 + 3;
+                        break;
+                    case 'Y':
+                        $points += 2 + 6;
+                        break;
+                    case 'Z':
+                        $points += 3 + 0;
+                        break;
+                }
+                break;
+            
+            case 'B':
+                switch ($choices[1]) {
+                    case 'X':
+                        $points += 1 + 0;
+                        break;
+                    case 'Y':
+                        $points += 2 + 3;
+                        break;
+                    case 'Z':
+                        $points += 3 + 6;
+                        break;
+                }
+                break;
+            
+            case 'C':
+                switch ($choices[1]) {
+                    case 'X':
+                        $points += 1 + 6;
+                        break;
+                    case 'Y':
+                        $points += 2 + 0;
+                        break;
+                    case 'Z':
+                        $points += 3 + 3;
+                        break;
+                }
+                break;
+        }
+    }
+        
+    echo $points;
+    echo PHP_EOL;
